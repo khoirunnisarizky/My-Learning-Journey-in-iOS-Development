8db1f2d81b15f5abe9f0bbeae2db73b092d52454
Khoirunnisa' Rizky Noor Fatimah <49296015+khoirunnisarizky@users.noreply.github.com>
2019-05-07 08:42:08 +0700

    Add files via upload

---
+++ Auto Layout Calculator/Auto Layout Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Auto Layout Calculator
+//
+//  Created by khoirunnisa' rizky noor fatimah on 05/05/19.
+//  Copyright © 2019 khoirunnisa' rizky noor fatimah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
